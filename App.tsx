49717712316ff6c695fa846153c6448a8b8c6a8c
Darshan Ponikar <darshanp@geekyants.com>
2022-07-29 17:15:31 +0530

    feat: listener added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, Linking, Text, TouchableOpacity, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 function HomeScreen() {
+  const onPress = async () => {
+    try {
+      await Linking.openURL('http://mytubesite.com/home/2');
+    } catch (e) {
+      console.log('E', e);
+    }
+  };
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Home Screen</Text>
+      <TouchableOpacity onPress={onPress}>
+        <Text>CLICK ME!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,6 +33,16 @@ const linking = {
 };
 
 function App() {
+  useEffect(() => {
+    Linking.addEventListener('url', e => {
+      console.log('FOUND URL', e);
+    });
+    return () => {
+      // Todo: remove this
+      Linking.removeAllListeners('url');
+    };
+  }, []);
+
   return (
     <NavigationContainer linking={linking}>
       <Stack.Navigator>
